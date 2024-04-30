530961647c78f591241d6246d29f99e3f691abb0
AsfaltMaple <solentuna@gmail.com>
2024-04-30 05:39:26 -0400

    text test with length

--- speedAnalysis/speed_analysis.js
+++ speedAnalysis/speed_analysis.js
@@ -29,6 +29,8 @@ function endTest() {
     var typedWords = userTypedText.split(/\s+/).filter(function (word) {
         return word !== "";
     }).length;
+
+    var typedSymbols = userTypedText.length;
     
     var wpm = 0; // Default value
     
@@ -39,6 +41,7 @@ function endTest() {
     // Display the results
     var outputDiv = document.getElementById("output");
     outputDiv.innerHTML = "<h2>Typing Test Results:</h2>" +
+        "<p>Total Length: " + typedSymbols + "</p>" +
         "<p>Words Typed: " + typedWords + "</p>" +
         "<p>Time Elapsed: " + timeElapsed.toFixed(2) + " seconds</p>" +
         "<p>Words Per Minute (WPM): " + wpm + "</p>";
