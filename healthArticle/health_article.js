5abdb7edfdc267aeca14fbcf91bf5855560268e9
AsfaltMaple <solentuna@gmail.com>
2024-05-13 09:39:38 -0400

    article temp2

---
+++ healthArticle/health_article.js
@@ -0,0 +1,10 @@
+var xhr = new XMLHttpRequest();
+var url = './health.json';
+
+xhr.open('GET', url, true);
+xhr.responseType = 'json';
+
+xhr.onload = function() {
+    var articles = xhr.response.articles;
+var articlesDiv = document.getElementById('articles');
+}
